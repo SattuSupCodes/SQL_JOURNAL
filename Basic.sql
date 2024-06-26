19fb877b47155fbeab48ae753536f4a17446dae0
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-26 15:07:46 +0530

    first commit

---
+++ Basic.sql
@@ -0,0 +1,11 @@
+/* We look into the basics of SQL starting from basic keywords and elements of the language*/
+
+SELECT --extracts data from database
+UPDATE --updates data
+DELETE --deletes data
+INSERT INTO --inserts new data
+CREATE DATABASE --creates new database
+ALTER DATABASE -- modifies database
+DROP TABLE -- deletes a table
+DROP INDEX -- deletes index
+CREATE INDEX --creates index
